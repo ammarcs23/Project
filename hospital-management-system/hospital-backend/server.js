972650d58646646905724ed138a48ba7fd2ae6cc
Ammar Jahangir <ammarmalik5535@gmail.com>
2026-04-07 15:30:29 +0500

    Changes

--- hospital-management-system/hospital-backend/server.js
+++ hospital-management-system/hospital-backend/server.js
@@ -31,7 +31,7 @@ app.use((req, res) => {
     res.status(404).json({ success: false, message: `Route ${req.originalUrl} not found.` });
 });
 
-// ── Error ─────────────────────────────────────
+// ── Error ─────────────────────
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ success: false, message: err.message || 'Server error.' });
